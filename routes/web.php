dd0026b94d1429f426d5b6f8596c982895b0fb94
Caner Cenk Kaya <canercenk.kaya@gmail.com>
2025-07-23 21:25:48 +0300

    route düzeltildi, artisan migration temiz deploy

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,17 @@
+<?php
+
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Artisan;
+
+Route::get('/', function () {
+    return view('welcome');
+});
 
 Route::get('/run-migrations', function () {
     try {
         Artisan::call('migrate', ['--force' => true]);
-        $output = Artisan::output(); // Çıktıyı al
-        return '<pre>' . $output . '</pre>'; // HTML çıktısı
+        $output = Artisan::output();
+        return '<pre>' . $output . '</pre>';
     } catch (\Exception $e) {
         return '<pre>HATA: ' . $e->getMessage() . "\n" . $e->getTraceAsString() . '</pre>';
     }
